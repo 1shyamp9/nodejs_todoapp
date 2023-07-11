e517be0f5161d51820e6cf16c052cfb1121ed455
1shyamp9 <1shyamp9@gmail.com>
2023-07-11 19:25:49 +0530

    Update Default Page

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ import express from 'express';
 import userRouter from './routes/usersRoutes.js';
 import taskRouter from './routes/taskRouter.js';
 import { configDotenv } from 'dotenv';
+import path from 'path';
 import cookieParser from 'cookie-parser';
 import {errorMiddleware } from './middlewares/error.js';
 import cors from 'cors';
 
 export const app = express();
+
+app.use(express.static(path.join(path.resolve(), "public"))) 
+
 app.use(express.json());
 app.use(cookieParser())
 configDotenv({
@@ -21,3 +25,7 @@ app.use("/api/v1/users",userRouter);
 app.use("/api/v1/tasks",taskRouter);
 
 app.use(errorMiddleware)
+
+app.get('/', (req,res)=>{
+    res.sendFile("index");
+})
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { app } from "./app.js";
 import { mongoDB } from "./database/database.js";
 
+
 mongoDB;
 app.listen(process.env.PORT, () => {
     console.log(`Server is Running.. on ${process.env.PORT} in ${process.env.NODE_ENV} Mode`);
