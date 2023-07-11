765fd40200c9c419bb9d97f808d76036094324ce
1shyamp9 <1shyamp9@gmail.com>
2023-07-11 17:43:13 +0530

    initial Commit

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const mongoDB = mongoose.connect(process.env.MONGO_URI,{
     dbName:"BackendTodo",
 })
-    .then(() => {
-        console.log("Connected to MondoDB");
+    .then((c) => {
+        console.log(`Connected to MondoDB ${c.connection.host}`);
     })
     .catch((e) => { console.log(e); })
